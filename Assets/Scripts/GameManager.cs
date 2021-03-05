5d8e294b989cdf7b2435183162b3d00d8acd7e4e
Ayhan Kara <78213760+iayka@users.noreply.github.com>
2021-03-05 20:42:00 +0300

    Player Movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool isStarted = false;
+    private bool isNotStarted = false;
+
+    private void Update()
+    {
+        if (!isNotStarted && Input.GetMouseButtonDown(0))
+        {
+            isStarted = true;
+            isNotStarted = true;
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+        GameManager.isStarted = false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public int woodCount;
+    float mouseStart;
+    public float deadZone = 0.1f;
+    public float horizontalSpeed;
+    Vector3 movementDelta;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (!GameManager.isStarted)
+            return;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!GameManager.isStarted)
+            return;
+        movementDelta = Vector3.forward * speed;
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseStart = Input.mousePosition.x;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float delta = Input.mousePosition.x - mouseStart;
+            mouseStart = Input.mousePosition.x;
+            if (Mathf.Abs(delta) <= deadZone)
+            {
+                delta = 0;
+            }
+            else
+            {
+                delta = Mathf.Sign(delta);
+            }
+            movementDelta += Vector3.right * horizontalSpeed * delta;
+        }
+        Move();
+    }
+
+    void Move()
+    {
+        rb.MovePosition(rb.position + movementDelta * Time.deltaTime);
+    }
+}
