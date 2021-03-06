87dd9048040c4c0e7a2307dbc5cdb6bd43937760
Ayhan Kara <78213760+iayka@users.noreply.github.com>
2021-03-06 15:32:53 +0300

    Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,17 @@ public class GameManager : MonoBehaviour
 {
     public static bool isStarted = false;
     private bool isNotStarted = false;
+    private bool isSettingsActive = false;
+    private bool isSoundActive = false;
+    private bool isVibrateActive = false;
+
+    public GameObject settings;
+    public GameObject settingsPanel;
+    public GameObject settingsPanelOffButton;
+    public GameObject soundOnButton;
+    public GameObject soundOffButton;
+    public GameObject vibrateOnButton;
+    public GameObject vibrateOffButton;
 
     private void Update()
     {
@@ -19,5 +30,29 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
         GameManager.isStarted = false;
+    }
+
+    public void Settings()
+    {
+        settingsPanel.SetActive(!isSettingsActive);
+        isSettingsActive = !isSettingsActive;
+    }
+
+    public void SettingsPanelOff()
+    {
+        settingsPanel.SetActive(!isSettingsActive);
+        isSettingsActive = !isSettingsActive;
+    }
+
+    public void SoundButton()
+    {
+        soundOffButton.SetActive(!isSoundActive);
+        isSoundActive = !isSoundActive;
+    }
+
+    public void Vibrate()
+    {
+        vibrateOffButton.SetActive(!isVibrateActive);
+        isVibrateActive = !isVibrateActive;
     }
 }
