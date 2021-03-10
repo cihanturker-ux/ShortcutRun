8b5408b8a1df6bd36f6f89cb9a25ee6cb8c4156c
Ayhan Kara <78213760+iayka@users.noreply.github.com>
2021-03-10 17:53:46 +0300

    Position System

---
+++ Assets/Scripts/Positions/PosDown1st.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PosDown1st : MonoBehaviour
+{
+    public GameObject positionDisplay;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Position")
+        {
+            positionDisplay.GetComponent<Text>().text = "2nd";
+        }
+    }
+}
